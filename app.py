0191fcb15fe215796f1be6045d5e94fd3b93dac2
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-03-17 14:53:24 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+app.config['TESTING'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+@app.route('/')
+def cchs_chart():
+    return render_template('index.html', title='CCHS Usual intakes from food, 2015')
+
+
+@app.route('/usual_intakes/about')
+def about():
+    return render_template('cchs_usual_intakes_about-en.html', title='CCHS Usual Intakes From Food, 2015')
+
+
+@app.route('/usual_intakes/data_table')
+def data_table():
+    return render_template('cchs_usual_intakes_table-en.html', title='Data Table - CCHS Usual Intakes From Food, 2015')
+
+
+@app.route('/usual_intakes/distribution')
+def distribution():
+    return render_template('cchs_usual_intakes_distribution-en.html', title='Distribution Curves - CCHS Usual Intakes From Food, 2015')
+
+
+@app.route('/usual_intakes/geographic')
+def geographic():
+    return render_template('cchs_usual_intakes_geographic-en.html', title='Geographic Comparison - CCHS Usual Intakes From Food, 2015')
+
+
+if __name__ == '__main__':
+    app.run()
