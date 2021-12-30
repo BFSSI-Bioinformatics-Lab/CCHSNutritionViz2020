e87b0dd2437db2d5d1fb5fe0cdeea09d405e23f1
stephanie <bfssi.stephanie.diaz@gmail.com>
2021-12-30 12:16:57 -0500

    change app routes and update routes in english html

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 #     return render_template('index.html', title='CCHS Usual intakes from food, 2015')
 
 
-@app.route('/')
+@app.route('/en/nutrition/usual-intakes')
 def about():
     return render_template('cchs_usual_intakes_about-en.html', title='CCHS Usual Intakes From Food, 2015')
 
@@ -22,49 +22,49 @@ def about():
 #     return render_template('cchs_usual_intakes_about-en.html', title='CCHS Usual Intakes From Food, 2015')
 #
 
-@app.route('/data_table')
+@app.route('/en/nutrition/usual-intakes/data-table')
 def data_table():
     return render_template('cchs_usual_intakes_table-en.html', title='Data Table - CCHS Usual Intakes From Food, 2015')
 
 
-@app.route('/distribution')
+@app.route('/en/nutrition/usual-intakes/distribution')
 def distribution():
     return render_template('cchs_usual_intakes_distribution-en.html',
                            title='Distribution Curves - CCHS Usual Intakes From Food, 2015')
 
 
-@app.route('/geographic')
+@app.route('/en/nutrition/usual-intakes/geographic')
 def geographic():
     return render_template('cchs_usual_intakes_geographic-en.html',
                            title='Geographic Comparison - CCHS Usual Intakes From Food, 2015')
 
-@app.route('/technical_notes')
+@app.route('/en/nutrition/usual-intakes/technical-notes')
 def technical_notes():
     return render_template('cchs_usual_intakes_technical_notes-en.html',
                            title='Technical Notes - CCHS Usual Intakes From Food, 2015')
 
 # French
-@app.route('/a_propos')
+@app.route('/fr/nutrition/apports-usuels')
 def a_propos():
     return render_template('escc_a_propos-fr.html', title='ESCC – Nutrition 2015')
 
-@app.route('/distribution_fr')
+@app.route('/fr/nutrition/apports-usuels/distribution')
 def distribution_fr():
     return render_template('escc_distribution-fr.html',
                            title='Courbes de distribution - ESCC – Nutrition 2015')
 
 
-@app.route('/geographique')
+@app.route('/fr/nutrition/apports-usuels/geographique')
 def geographique():
     return render_template('escc_geographique-fr.html',
                            title='Comparaison géographique - ESCC – Nutrition 2015')
 
-@app.route('/tableau')
+@app.route('/fr/nutrition/apports-usuels/tableau')
 def tableau():
     return render_template('escc_tableau-fr.html',
                            title='Tableau de données - ESCC – Nutrition 2015')
 
-@app.route('/notes_techniques')
+@app.route('/fr/nutrition/apports-usuels/notes-techniques')
 def notes_techniques():
     return render_template('escc_notes_techniques-fr.html',
                            title='Notes techniques - ESCC – Nutrition 2015')
